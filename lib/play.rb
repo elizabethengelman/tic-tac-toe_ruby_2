5619064d5f7d7658159ea11c25e1e76741cec2ef
Elizabeth Engelman <elizabethengelman@Elizabeths-MacBook-Pro.local>
2014-01-09 16:30:32 -0600

    added funcationality to allow the user to choose who goes first

--- lib/play.rb
+++ lib/play.rb
@@ -1,17 +1,19 @@
 class Play
+	attr_reader :user #added this so that I could test that a new user is created when
+										#a new Play object is initialized - this seems like cheating
 	def initialize
 		@user = UserInterface.new
 	end
   
 	def start_playing
-		response = "yes"
-		until response != "yes"
-      @board = Board.new
+	  response = "yes"
+	  until response != "yes"
+    	@board = Board.new
       @game = Game.new(@board, @user)
 			@game.play_game
 			@user.print_out("Game over! Would you like to start a new game?")
 			response = @user.get_input
-		end
-		@user.print_out("Thanks for playing! Goodbye!")
+	  end
+	  @user.print_out("Thanks for playing! Goodbye!")
 	end
 end
--- spec/play_spec.rb
+++ spec/play_spec.rb
@@ -8,6 +8,29 @@ describe Play do
 	describe "#new" do
 		it "should create a play object" do
 			@play.should be_an_instance_of Play 
-	  end
+   	end
+
+   	it "should initialize with a new UserInterface object" do
+     @play.user.should be_an_instance_of UserInterface
+   	end
+	end
+
+	describe "#start_playing" do
+	
+		# it "should create a new Board" do
+
+		# end
+
+		# it "should create a new Game" do 
+		# 	pending
+		# end
+    
+  #   it "should call the play_game method on the @game object" do
+  #   	pending
+  #   end
+    
+		it "should not repeat the loop if the user input is 'no'" do
+			pending
+		end
 	end
 end
